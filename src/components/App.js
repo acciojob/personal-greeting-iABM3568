3814232432acd012ca8492c15b1abb84e1399dfe
Abhishek Mishra <iabm3568@gmail.com>
2025-12-29 22:38:32 +0530

    Refactor App component to use useState for name input

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
+import '../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input 
+        type="text" 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      {name && <p>Hello, {name}!</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
